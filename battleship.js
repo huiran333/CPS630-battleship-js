78d98f67e9e56c6a6ff46280eed25250a3bf7b47
huiran333 <huiran.tang@torontomu.ca>
2024-02-04 17:39:48 -0500

    update

--- battleship.js
+++ battleship.js
@@ -6,11 +6,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const mctx = myCanvas.getContext('2d');
     const gridSize = 10;
     const cellSize = canvas.width / gridSize;
-    const btnStart = document.getElementById('btnStart');
     let ships = [];
     let hits = [];
     let drownship = 0;
     let myShips = [];
+
+    let myTurn = false;
 
     function getRndInteger(min, max) 
     {
@@ -26,8 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
             let num2 = getRndInteger(0,9);
             let temp = [];
             temp[0] = num1;
-            temp[1] = num2;
-            console.log(temp);
+            temp[1] = num2;            
             if (!ships.includes(temp))
             {
                 ships[ships.length] = temp;
@@ -37,7 +37,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 i--;
             }
         }
-        console.log(ships);
     }
 
     function drawBoard() {
@@ -56,29 +55,66 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    function handleCanvasClick(event) {
-        const rect = canvas.getBoundingClientRect();
-        const x = event.clientX - rect.left;
-        const y = event.clientY - rect.top;
-        const gridX = Math.floor(x / cellSize);
-        const gridY = Math.floor(y / cellSize);
-
-        if (!hits.some(hit => hit[0] === gridX && hit[1] === gridY)) {
-            if (ships.some(ship => ship[0] === gridX && ship[1] === gridY)) {
-                ctx.fillStyle = 'red';
-                ctx.fillRect(gridX * cellSize, gridY * cellSize, cellSize, cellSize);
-                hits.push([gridX, gridY]);
-                drawnship+=1;
-                if(drownship == 5)
-                {
-                    initGame();
-                }
-            } else {
-                ctx.fillStyle = 'blue';
-                ctx.fillRect(gridX * cellSize, gridY * cellSize, cellSize, cellSize);
-                hits.push([gridX, gridY]);
-            }
+    function enemyTurn()
+    {
+        let gridX = getRndInteger(0,9);
+        let gridY = getRndInteger(0,9);     
+        var gridColor = mctx.getImageData(gridX * cellSize, gridY * cellSize, cellSize, cellSize).data;
+        console.log("color:" + gridColor[1]);
+        if (gridColor[1] == 251)//green
+        {
+            mctx.fillStyle = 'Crimson';
+            mctx.fillRect(gridX * cellSize, gridY * cellSize, cellSize, cellSize);
+            myTurn = true;   
+            console.log("bomed:" + gridX + "," + gridY); 
         }
+        else if(gridColor[1] == 191 || gridColor[1] == 20)//blue or red
+        {
+            console.log("try again");
+            enemyTurn();
+        }        
+        else
+        {
+            mctx.fillStyle = 'DeepSkyBlue';
+            mctx.fillRect(gridX * cellSize, gridY * cellSize, cellSize, cellSize);
+            myTurn = true;    
+            console.log("bomed missed:" + gridX + "," + gridY);    
+        }
+        
+    }
+
+    function handleCanvasClick(event) {
+        if(myTurn)
+        {
+            const rect = canvas.getBoundingClientRect();
+            const x = event.clientX - rect.left;
+            const y = event.clientY - rect.top;
+            const gridX = Math.floor(x / cellSize);
+            const gridY = Math.floor(y / cellSize);
+
+            if (!hits.some(hit => hit[0] === gridX && hit[1] === gridY)) {
+                if (ships.some(ship => ship[0] === gridX && ship[1] === gridY)) {
+                    ctx.fillStyle = 'Crimson';
+                    ctx.fillRect(gridX * cellSize, gridY * cellSize, cellSize, cellSize);
+                    hits.push([gridX, gridY]);
+                    drownship+=1;
+                    if(drownship == 5)
+                    {
+                        initGame();
+                    }
+                } else {
+                    ctx.fillStyle = 'DeepSkyBlue';
+                    ctx.fillRect(gridX * cellSize, gridY * cellSize, cellSize, cellSize);
+                    hits.push([gridX, gridY]);
+                }
+            }
+            else
+            {
+                window.alert("You already bombed here!");
+            }
+            myTurn = false;
+            enemyTurn();
+        }        
     }
 
     function removeship(i1,i2)
@@ -97,22 +133,19 @@ document.addEventListener('DOMContentLoaded', function() {
         const y = event.clientY - rect.top;
         const gridX = Math.floor(x / cellSize);
         const gridY = Math.floor(y / cellSize);     
-
         if (!myShips.some(myship => myship[0] === gridX && myship[1] === gridY))//false = no ship
-        {
-            console.log("empty");
+        {            
             if (myShips.length < 5)
             {
-                mctx.fillStyle = 'green';
+                mctx.fillStyle = 'PaleGreen';
                 mctx.fillRect(gridX * cellSize, gridY * cellSize, cellSize, cellSize);
-                 myShips.push([gridX,gridY]);
+                myShips.push([gridX,gridY]);
             }            
         }
         else//There is ship
-        {
-            console.log("ship");
+        {            
             removeship(gridX,gridY);
-            mctx.clearRect(gridX * cellSize, gridY * cellSize, cellSize, cellSize)            
+            mctx.clearRect(gridX * cellSize, gridY * cellSize, cellSize, cellSize);
             
         }
 
@@ -127,8 +160,14 @@ document.addEventListener('DOMContentLoaded', function() {
        console.log(myShips);
     }
 
+    function btnStartEvent()
+    {
+        myTurn = true;
+    }
+
     function initGame() {
         document.getElementById("btnStart").disabled = true
+        document.getElementById("btnStart").addEventListener('click',btnStartEvent);
         drawBoard();        
         randomShips();
         canvas.addEventListener('click', handleCanvasClick);
